7b5c7d81931ee3feb4ea70b99efaedb6ece0a9db
Logan Negley <logannegley@gmail.com>
2020-07-08 20:04:58 -0500

    binary_search passes tests

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -8,9 +8,22 @@ def linear_search(arr, target):
 
 
 # Write an iterative implementation of Binary Search
+arr = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
 def binary_search(arr, target):
+    start = 0
+    end = len(arr) - 1
 
-    # Your code here
-
-
+    found = False
+    while end >= start and not found:
+        mid = (end + start) // 2
+        if arr[mid] == target:
+            found = True
+            return arr.index(arr[mid])
+        else:
+            if arr[mid] > target:
+                end = mid - 1
+            if arr[mid] < target:
+                start = mid + 1
     return -1  # not found
+
+binary_search(arr, 8)
